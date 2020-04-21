fbf8fc4cedf3cb55c71cda0f7f582c7b50891319
gsnelson <58017317+gsnelson@users.noreply.github.com>
2020-04-21 11:43:59 -0400

    initial commit for plot2.R refinement to plot1.R

--- plot2.R
+++ plot2.R
@@ -3,10 +3,17 @@ NEI <- readRDS(".data/summarySCC_PM25.rds")
 SCC <- readRDS(".data/Source_Classification_Code.rds")
 
 
-## sum emissions by year
-pm25_yr_totals <- aggregate(x = NEI$Emissions,
-							by = list(NEI$year),
-							FUN = sum)
+## subset on Baltimore City fips code
+balti <- subset(NEI, fips == "24510")
+
+
+## sum Baltimore City emissions by year
+pm25_balti_totals <- aggregate(
+	x = balti$Emissions,
+	by = list(balti$year),
+	FUN = sum,
+	drop = TRUE
+)
 
 
 # remove variables no longer needed
@@ -15,38 +22,35 @@ rm(NEI, SCC)
 
 ## set the graphics parameter to PNG graphics device
 png(
-	filename = "plot1.png",
+	filename = "plot2.png",
 	width = 800,
 	height = 600,
 	units = "px"
 )
+
 
 ## define plot margins
 ## plot line chart
 ## annotate chart
 ## define axes
 ## add reference lines
-par(mar = c(5, 5, 3, 2), las = 1)
+par(mar = c(5, 6, 4, 1), las = 1)
 plot(
-	pm25_yr_totals$Group.1,
-	pm25_yr_totals$x,
+	pm25_balti_totals$Group.1,
+	pm25_balti_totals$x,
 	type = "b",
 	lwd = 2,
 	pch = 19,
 	xaxt = "n",
 	yaxt = "n",
 	xlab = "Measurement Year",
-	ylab = "Total Fine Particulate Matter (PM2.5)\n(Tons in Millions)",
-	main = "Total Fine Particulate Matter (PM2.5) By Measurement Year"
+	ylab = "Total Fine Particulate Matter (PM2.5) (in tons)\n",
+	main = "Total Fine Particulate Matter (PM2.5) By Measurement Year\nCity of Baltimore, Maryland"
 )
 axis(1, xaxp = c(1999, 2008, 3))
-axis(
-	2,
-	at = c(3500000, 4500000, 5500000, 6500000, 7250000),
-	labels = c(3.50, 4.50, 5.50, 6.50, 7.25)
-)
+axis(2)
 abline(
-	h = c(3500000, 4500000, 5500000, 6500000, 7250000),
+	h = c(2000, 2200, 2400, 2600, 2800, 3000, 3200),
 	v = 1999:2008,
 	col = "lightgray",
 	lty = 3
@@ -54,7 +58,7 @@ abline(
 
 
 # remove variables no longer needed
-rm(pm25_yr_totals)
+rm(balti, pm25_balti_totals)
 
 ## close the PNG device
 dev.off()
